bedfd56c66f5829aaccccd5f01dcde1c89dabda7
Tiffany Liang <tiffanyliang@Tiffanys-MacBook-Air.local>
2021-05-06 19:23:38 -0400

    added header and imported packages

--- BenfordsLaw.java
+++ BenfordsLaw.java
@@ -1,3 +1,14 @@
+/*
+ * Date: May 6, 2021
+ * Names: Johnny He and Tiffany Liang
+ * Teacher: Mr. Ho
+ * Description: A sales analysis system that allows the user to load the sales data, checks if fraud has likely occured or not, 
+ *              and prints a graph + table of the first digit distribution results 
+ * */
+
+import java.io.File;
+import java.io.FileNotFoundException; 
+import java.util.Scanner;
 class BenfordsLaw{
     public static void main(String[] args){
         
